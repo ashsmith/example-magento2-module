5d745c501eba25b54c1bdf14fc2181efb89c9637
Nathan McBride <nathan@brideo.co.uk>
2016-05-16 09:38:50 +0100

    Setup initial Magento 2 module structure

---
+++ README.md
@@ -0,0 +1,8 @@
+# Meanbee Example Magento 2 Module
+
+This module is responsible to show Meanbee's module layout structure.
+
+## Testing
+
+    docker-compose run cli composer install
+    docker-compose run cli ./vendor/bin/phpunit
---
+++ src/Test/Unit/ModuleSetupTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Meanbee\ExampleMagento2Module\Test\Unit;
+
+use PHPUnit_Framework_TestCase;
+
+class VariableFileTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     */
+    public function testModuleSetup()
+    {
+        $this->assertTrue(true);
+    }
+}

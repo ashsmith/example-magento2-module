201a6210dd5ba99a7674d45fad13883c8c94c5a7
Nathan McBride <nathan@brideo.co.uk>
2016-05-16 09:49:43 +0100

    Update test class name

--- src/Test/Unit/ModuleSetupTest.php
+++ src/Test/Unit/ModuleSetupTest.php
@@ -4,7 +4,7 @@ namespace Meanbee\ExampleMagento2Module\Test\Unit;
 
 use PHPUnit_Framework_TestCase;
 
-class VariableFileTest extends PHPUnit_Framework_TestCase
+class ModuleSetupTest extends PHPUnit_Framework_TestCase
 {
 
     /**
